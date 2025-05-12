8adda03d204b163f5c333bba726a5c5f873bde42
Ksenia Almametova <almaxana_04@mail.ru>
2025-05-12 08:26:45 +0300

    add DB models

--- timesheet/main/admin.py
+++ timesheet/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Worker, Project, TimeSheetItem
+
+admin.site.register(Worker)
+admin.site.register(Project)
+admin.site.register(TimeSheetItem)
--- timesheet/main/models.py
+++ timesheet/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker(models.Model):
+    first_name = models.CharField("Имя", max_length=50)
+    last_name = models.CharField("Фамилия", max_length=50)
+    position = models.CharField("Должность", max_length=100)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} — {self.position}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    customer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class TimeSheetItem(models.Model):
+    date = models.DateField()
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE, related_name='timesheet_items')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='timesheet_items')
+    hours_number = models.PositiveIntegerField()
+    comment = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.date} - {self.worker.name} - {self.project.name} - {self.hours_number}h"
