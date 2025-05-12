f18af4a02e8539041946b353c53fb7f8e89e4bf0
Ksenia Almametova <almaxana_04@mail.ru>
2025-05-12 13:10:50 +0300

    clean

--- timesheet/main/models.py
+++ timesheet/main/models.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Worker(models.Model):
-    first_name = models.CharField("Имя", max_length=50)
-    last_name = models.CharField("Фамилия", max_length=50)
     position = models.CharField("Должность", max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.last_name} {self.first_name}"
+        return f"{self.last_name}"
 
 
 class Project(models.Model):
--- timesheet/main/views.py
+++ timesheet/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
 from .models import TimeSheetItem
 
 
-def main_home(request):
-    timeSheet = TimeSheetItem.objects.order_by('-date')
-    return render(request, 'main/main_home.html', {'timeSheet': timeSheet})
+def home(request):
+    timesheet = TimeSheetItem.objects.order_by('-date')
+    return render(request, 'main/home.html', {'timeSheet': timesheet})
+
+
+
