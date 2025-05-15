8fb06cdcf1d5d94747012498cdd0e2ed5c6f0af3
Ksenia Almametova <almaxana_04@mail.ru>
2025-05-15 14:08:36 +0300

    login

--- timesheet/main/urls.py
+++ timesheet/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +8,6 @@ urlpatterns = [
     path('add-entry/', views.add_timesheet_entry, name='add_timesheet_entry'),
     path('delete-entry/<int:entry_id>/', views.delete_timesheet_entry, name='delete_timesheet_entry'),
     path('update-entry/<int:entry_id>/', views.update_timesheet_entry, name='update_timesheet_entry'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout')
 ]
--- timesheet/timesheet/settings.py
+++ timesheet/timesheet/settings.py
@@ -117,3 +117,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'main.Worker'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
